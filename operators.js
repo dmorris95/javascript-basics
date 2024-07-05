db0bb9f2ebb93324afe5419dc8c17dfc2d376fd1
dmorris95 <cardinalfanfrommi@gmail.com>
2024-07-05 13:39:12 -0500

    first commit

---
+++ operators.js
@@ -0,0 +1,33 @@
+//Task 1
+let x, y;
+
+//Task 2
+x = 4;
+y = 9;
+
+//Task 3
+
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+//Task 4
+
+console.log("Updated X to:", x = 6);
+console.log("Updated Y to:", y = 13);
+
+//Task 5
+
+console.log("Is X equal to Y?", x == y);
+console.log("Is X greater than Y", x > y);
+console.log("Is X not equal to Y?", x != y);
+console.log("Is X less than Y?", x < y);
+
+//Task 6(
+
+let isPositive = (x > 0) && (y > 0);
+let isEven = (x % 2 == 0) || (y % 2 == 0);
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is one of the numbers even?", isEven);
---
+++ script.js
@@ -0,0 +1,15 @@
+//Task 1
+
+let studentName, studentAge, studentGrade, isAttending;
+
+//Task 2
+studentAge = 16;
+studentGrade = 8;
+studentName = "George";
+isAttending = false;
+
+//Task 3
+console.log(studentName);
+console.log(studentAge);
+console.log(studentGrade);
+console.log(isAttending);
